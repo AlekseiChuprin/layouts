a86ab2835df487799f533fd9ad9ba596eba08372
Chuprin Alexey <chuprin.aleksei1504@gmail.com>
2021-06-04 15:20:01 +0300

    layouts

---
+++ Block/HelloWorld.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Study\LayoutBlock\Block;
+
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+    public function helloWorld1()
+    {
+        return __('Hello World1');
+    }
+
+    public function helloWorld2()
+    {
+        return __('Hello World2');
+    }
+
+    public function helloWorld3()
+    {
+        return __('Hello World3');
+    }
+
+    public function helloWorld4()
+    {
+        return __('Hello World4');
+    }
+
+    public function helloWorld5()
+    {
+        return __('Hello World5');
+    }
+
+    public function helloWorld6()
+    {
+        return __('Hello World6');
+    }
+
+    public function helloWorld7()
+    {
+        return __('Hello World7');
+    }
+
+    public function helloWorld8()
+    {
+        return __('Hello World8');
+    }
+
+    public function helloWorld9()
+    {
+        return __('Hello World9');
+    }
+}
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Study_LayoutBlock',
+    __DIR__
+);
+
